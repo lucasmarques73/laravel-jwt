919860fbedb36d0b32f944864b665e631d28bf6e
Lucas Marques <lucasmarques73@hotmail.com>
2018-07-15 20:09:04 -0300

    migrate:seed

--- api/database/seeds/UsersTableSeeder.php
+++ api/database/seeds/UsersTableSeeder.php
@@ -11,6 +11,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        factory(\App\Entities\User::class,4)->create();
+        factory(\App\Entities\User::class)->create([
+            'name' => 'Lucas Marques',
+            'email' => 'lucasmarques73@hotmail.com',
+            'password' => bcrypt('secret'),
+            'remember_token' => str_random(10),
+        ]);
     }
 }
